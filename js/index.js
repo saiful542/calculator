d21f2cac0d793379195429f56f6842f0a5877da9
saiful542 <saifuliiuc12@gmail.com>
2022-10-13 22:06:37 +0600

    3

--- js/index.js
+++ js/index.js
@@ -7,10 +7,17 @@ const insert = (inserted_value) => {
     // if (isNaN(parseFloat(inserted_value))) {
     //     calculation(inserted_value, updated_value)
     // }
+    if (inserted_value != '=' && inserted_value != 'c' && inserted_value != 'del') {
+        console.log(updated_value)
+        updated_value += inserted_value
+        display_calculation.value = updated_value;
+        final_answer(eval(updated_value))
+    }
 
-    updated_value = updated_value + inserted_value
-    display_calculation.value = updated_value;
-    final_answer(eval(updated_value))
+    if (inserted_value == '=' || inserted_value == 'c' || inserted_value == 'del') {
+        optional_calculation(inserted_value, updated_value)
+
+    }
 
 }
 
@@ -23,7 +30,17 @@ const insert = (inserted_value) => {
 //     }
 // }
 
-
+const optional_calculation = (inserted_value, updated_value) => {
+    if (inserted_value === '=' || inserted_value === 'c') {
+        updated_value = ''
+        display_calculation.value = '';
+        console.log(inserted_value)
+    }
+    else {
+        updated_value = updated_value.slice(0, -1)
+        display_calculation.value = updated_value
+    }
+}
 
 
 const final_answer = (answer) => {
