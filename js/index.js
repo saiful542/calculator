8decedc860e6cd3f008c886c42bc0159a9966a5e
saiful542 <saifuliiuc12@gmail.com>
2022-10-18 22:01:06 +0600

    s

--- js/index.js
+++ js/index.js
@@ -5,122 +5,98 @@ let display_answer = document.querySelector('.display .answer')
 let answer = 0;
 let dot_counter = 0;
 let i = 0;
-let reducer_counter = 0
+let reducer_counter = 0;
+let sum = 0;
+let calculation_value = ''
+let change_value = ''
 const insert = (inserted_value) => {
-    // if (isNaN(parseFloat(inserted_value))) {
-    //     calculation(inserted_value, show_value)
-    // }
-    // console.log(inserted_value)
     if (inserted_value == '=' || inserted_value == 'c' || inserted_value == 'del') {
         optional_calculation(inserted_value)
     }
 
     if (inserted_value != '=' && inserted_value != 'c' && inserted_value != 'del') {
-        // if ((show_value[show_value.length - 1] != inserted_value)  && (!isNaN(inserted_value))) {
-        //     show_value += inserted_value
-        //     display_calculation.value = show_value;
-        //     // final_answer(eval(show_value))
-        //     calculation(show_value, inserted_value)
-        // }
 
         if (!(isNaN(show_value[show_value.length - 1]) && isNaN(inserted_value))) {
-            // if (inserted_value === '.') {
-            //     console.log(typeof(parseFloat(inserted_value)))
-            //     dot_counter++;
-            // }
-            // if (dot_counter <= 1) {
-
-            // }
-
 
             if (inserted_value == '.') {
                 dot_counter++;
             }
 
             if (dot_counter <= 1 || inserted_value != '.') {
-                show_value += inserted_value
+
+                change_value = inserted_value
+                calculation_value += inserted_value
+                if (change_value == '*')
+                    change_value = 'x'
+                show_value += change_value
                 display_calculation.value = show_value;
-                // final_answer(eval(show_value))
-                calculation(show_value, inserted_value)
+
+                calculation(calculation_value, inserted_value)
             }
-
-
-
         }
     }
     else
-        calculation(show_value, inserted_value);
+        calculation(calculation_value, inserted_value);
 
 
 
 }
 
-const calculation = (show_value, inserted_value) => {
+const calculation = (calculation_value, inserted_value) => {
+
     if (inserted_value == '+') {
-        console.log(add(show_value))
-        // console.log(parseFloat(show_value.split('+')[0]));
-        // console.log(show_value.split('+')[0])
+        // console.log(add(show_value))
         dot_counter = 0;
     }
     if (inserted_value == '-') {
-        console.log(subtraction(show_value))
+        // console.log(subtraction(show_value))
         dot_counter = 0;
     }
-    if (inserted_value == 'x') {
-        console.log(multiply(show_value))
+    if (inserted_value == '*') {
+        // console.log(multiply(show_value))
         dot_counter = 0;
     }
     if (inserted_value == '/') {
-        console.log(divide(show_value))
+        // console.log(divide(show_value))
         dot_counter = 0;
     }
 
-
+    final_answer(eval(calculation_value))
 }
-let sum = 0;
-const add = (show_value) => {
 
-    // updated_value = 
-    // let p = show_value.split('+').slice(0, -1).forEach(element => {
-    //     sum += parseFloat(element);
-    // });
-    if (reducer_counter == 0) {
-        console.log(i)
-        sum += parseFloat(show_value.split('+')[i]);
-        i++;
-        reducer_counter = 1;
-    }
+// const add = (show_value) => {
 
-    // updated_value = toString(sum);
-    return sum;
-}
-const subtraction = (show_value) => {
-    let sum = 0;
-    updated_value = show_value.split('-')
-    let p = updated_value.slice(0, -1).forEach(element => {
-        sum -= parseFloat(element);
-    });
+//     updated_value =
 
-    return sum;
-}
-const multiply = (show_value) => {
-    let sum = 0;
-    updated_value = show_value.split('+')
-    let p = updated_value.slice(0, -1).forEach(element => {
-        sum += parseFloat(element);
-    });
+//         console.log(sum)
+//     console.log(show_value.split('+').slice(0, -1))
 
-    return sum;
-}
-const divide = (show_value) => {
-    let sum = 0;
-    updated_value = show_value.split('+')
-    let p = updated_value.slice(0, -1).forEach(element => {
-        sum += parseFloat(element);
-    });
+//     show_value.split('+').slice(0, -1).forEach(element => {
+//         sum += parseFloat(element);
+//     });
 
-    return sum;
-}
+
+
+//     return sum;
+// }
+// const subtraction = (show_value) => {
+
+//     console.log(show_value.split('-').slice(0, -1))
+
+//     return sum;
+// }
+// const multiply = (show_value) => {
+
+//     console.log(show_value.split('x').slice(0, -1))
+
+//     return sum;
+// }
+// const divide = (show_value) => {
+
+//     console.log(show_value.split('/').slice(0, -1))
+
+//     return sum;
+// }
 
 
 
@@ -140,13 +116,16 @@ const optional_calculation = (inserted_value) => {
 const clear_display = () => {
     show_value = ' ';
     display_calculation.value = show_value;
-    final_answer('');
+    display_answer.value = ''
     clear_variables();
 }
 const clear_update = () => {
+    final_answer(eval(calculation_value))
     show_value = ' ';
     display_calculation.value = show_value;
+    console.log(calculation_value)
     clear_variables();
+    console.log(calculation_value)
 
 }
 
@@ -154,23 +133,35 @@ const reducer = () => {
     let new_value = show_value.slice(0, -1);
     show_value = new_value;
     display_calculation.value = new_value;
-    i--;
-    dot_counter = 0;
-    reducer_counter = 1;
-    return reducer_counter;
+    final_answer(eval(calculation_value))
+    // i--;
+
+    if (dot_counter > 0) {
+        dot_counter = 0;
+        console.log(dot_counter)
+    }
+
+    // reducer_counter = 1;
+    // return reducer_counter;
 }
 
 
 const final_answer = (answer) => {
+    console.log(answer)
+    if (answer == undefined)
+        answer = '0'
     display_answer.value = answer;
+
 }
 
 
 //clear all global variables
 const clear_variables = () => {
-    i = 0;
-    sum = 0;
-    reducer_counter = 0;
+    // i = 0;
+    // sum = 0;
+    // reducer_counter = 0;
     dot_counter = 0
+    calculation_value = '';
+
 }
 
